85dda68408574df11cc571f57c6f8adf0b6b030a
Steve Chavez <sting.chavez@gmail.com>
2016-09-12 18:00:55 -0500

    app.js created

---
+++ app.js
@@ -0,0 +1,21 @@
+const bodyParser = require('body-parser');
+var express = require('express');
+// create our app
+var app = express();
+
+// instruct the app to use the `bodyParser()` middleware for all routes
+/*app.use(bodyParser());*/
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+app.post('/reservation', function(req, res){
+  var name = JSON.stringify(req.body)
+  var html = `Hello ${name}!`
+  res.send(html);
+});
+
+app.listen(3000);
