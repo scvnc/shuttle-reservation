477a7ebff7d35fbe89f7d2129368b61d80d1a502
Steve Chavez <sting.chavez@gmail.com>
2016-09-12 19:13:52 -0500

    app file added

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
 app.post('/reservation', function(req, res){
   var name = JSON.stringify(req.body)
   var html = `Hello ${name}!`
   res.send(html);
 });
 
-app.listen(3000);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
